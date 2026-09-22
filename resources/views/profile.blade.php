6074ec4ab738ec20beb30cc89c7840ce6884278c
unknown <sasanafisa57@gmail.com>
2026-09-22 15:21:46 +0700

    route,view

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>profile</title>
+    <link rel="stylesheet" href="css/style.css">
+   
+</head>
+<body>
+    <h1>HALAMAN PROFILE</h1>
+    <P>
+        Nama: Viky Diana Nafisa <br>
+        Prodi: In TI 
+
+
+    </P>
+    <script scr="js/profile.js"></script>
+    <img src="images/unimus.png"></img>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/berita', function () {
+    return view('berita');
 });
